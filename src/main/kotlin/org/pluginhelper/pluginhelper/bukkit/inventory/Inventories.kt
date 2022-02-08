b4139c761b9c3bb04f7c626b326cf09080018166
acog <acog.seongmin@gmail.com>
2022-02-09 05:17:54 +0900

    Inventory Builder

---
+++ src/main/kotlin/org/pluginhelper/pluginhelper/bukkit/inventory/Inventories.kt
@@ -0,0 +1,6 @@
+package org.pluginhelper.pluginhelper.bukkit.inventory
+
+import org.bukkit.Bukkit
+import org.bukkit.inventory.Inventory
+
+fun inventory(title: String, row: Int = 1, c: Inventory.() -> Unit): Inventory = Bukkit.createInventory(null, row * 9).apply(c)
