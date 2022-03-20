4b016054052bd3716691330064a1efd3cac261f6
acog <acog.seongmin@gmail.com>
2022-03-21 07:56:10 +0900

    1.0.1 new Version PluginHelper

--- legacy/src/main/kotlin/org/pluginhelper/bukkit/string/Chats.kt
+++ legacy/src/main/kotlin/org/pluginhelper/bukkit/string/Chats.kt
@@ -4,9 +4,9 @@ import org.bukkit.ChatColor
 import org.bukkit.command.CommandSender
 import org.bukkit.entity.Player
 
-fun Player.toSend(message: String) = sendMessage(message)
+fun Player.toSend(message: String) = sendMessage(message.colorize())
 
-fun CommandSender.toSend(message: String) = sendMessage(message)
+fun CommandSender.toSend(message: String) = sendMessage(message.colorize())
 
 fun String.colorize(altColorChar: Char = '&'): String =
     ChatColor.translateAlternateColorCodes(altColorChar, this)
