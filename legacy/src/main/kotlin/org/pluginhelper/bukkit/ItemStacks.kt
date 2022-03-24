50a545681fcbf70f5d27754c1db061bab6ccee26
acog <acog.seongmin@gmail.com>
2022-03-25 07:51:17 +0900

    1.0.1 new Version PluginHelper

--- legacy/src/main/kotlin/org/pluginhelper/bukkit/ItemStacks.kt
+++ legacy/src/main/kotlin/org/pluginhelper/bukkit/ItemStacks.kt
@@ -4,21 +4,15 @@ import org.bukkit.Material
 import org.bukkit.inventory.ItemStack
 import org.bukkit.inventory.meta.ItemMeta
 
-fun Material.toItem(amount: Int = 1) = ItemStack(this, amount)
+fun Material.toItem(amount: Int = 1) : ItemStack = ItemStack(this, amount)
 
-fun item(material: Material, amount: Int = 1, c: ItemStack.() -> Unit = {}) = material.toItem(amount).apply(c)
+fun item(material: Material, amount: Int = 1, c: ItemStack.() -> Unit = {}) : ItemStack = material.toItem(amount).apply(c)
 
 fun item(itemStack: ItemStack, amount: Int = 1, c: ItemStack.() -> Unit = {}) =
     ItemStack(itemStack).apply {
         setAmount(amount)
         c()
     }
-
-fun ItemStack.setDisplayName(display: String) {
-    itemMeta?.displayName = display
-}
-
-fun ItemStack.setLore(lore: List<String>) { itemMeta?.lore = lore }
 
 fun ItemStack.setLore(vararg lore: String) { setLore(*lore) }
 
