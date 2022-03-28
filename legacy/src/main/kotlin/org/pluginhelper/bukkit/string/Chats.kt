3a89621ca87bbf3e0e1cdc902e143c13305c7b29
acog <acog.seongmin@gmail.com>
2022-03-28 16:34:20 +0900

    1.0.1 new Version PluginHelper

--- legacy/src/main/kotlin/org/pluginhelper/bukkit/string/Chats.kt
+++ legacy/src/main/kotlin/org/pluginhelper/bukkit/string/Chats.kt
@@ -3,6 +3,7 @@ package org.pluginhelper.bukkit.string
 import org.bukkit.ChatColor
 import org.bukkit.command.CommandSender
 import org.bukkit.entity.Player
+import java.text.DecimalFormat
 
 fun Player.toSend(message: String) = sendMessage(message.colorize())
 
@@ -10,3 +11,8 @@ fun CommandSender.toSend(message: String) = sendMessage(message.colorize())
 
 fun String.colorize(altColorChar: Char = '&'): String =
     ChatColor.translateAlternateColorCodes(altColorChar, this)
+
+fun <T> T.decimalFormat(): String {
+    val decimalFormat = DecimalFormat("#,###")
+    return decimalFormat.format(this)
+}
