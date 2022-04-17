7ff08ced7ca3ed2e0478fb2d47d57aad157ed909
acog <acog.seongmin@gmail.com>
2022-04-17 19:09:17 +0900

    new Exceptions#MessageException

--- legacy/src/main/kotlin/org/pluginhelper/bukkit/exceptions/Exceptions.kt
+++ legacy/src/main/kotlin/org/pluginhelper/bukkit/exceptions/Exceptions.kt
@@ -3,3 +3,5 @@ package org.pluginhelper.bukkit.exceptions
 open class CommandArgumentNullException(private val error: String) : Exception(error)
 
 open class PlayerCastingException(private val error: String) : Exception(error)
+
+open class MessageException(private val error: String) : Exception(error)
